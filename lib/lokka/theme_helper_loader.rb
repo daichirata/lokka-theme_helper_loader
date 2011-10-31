4a79a3e61a04bcbd2f063609ac8b26934c970c34
Dach-h <bunny.hop.md@gmail.com>
2011-10-31 11:09:11 +0900

    fix require path

---
+++ lib/lokka/theme_helper_loader.rb
@@ -0,0 +1,15 @@
+module Lokka
+  module ThemeHelperLoader
+    def self.registered(app)
+      names = []
+      Dir["#{Lokka.root}/public/theme/*/helper/*_helper.rb"].each do |path|
+        path = Pathname.new(path)
+        lib  = path.parent.parent
+        root = lib.parent
+        $:.push lib
+        name = path.basename.to_s.split('.').first
+        require "helper/#{name}"
+      end
+    end
+  end
+end
