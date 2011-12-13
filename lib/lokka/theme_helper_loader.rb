884c9c5a68938e1dcf9df1b63898771cf23dfb46
Dach-h <bunny.hop.md@gmail.com>
2011-12-13 09:59:35 +0900

    sentence which is not needed is deleted.

--- lib/lokka/theme_helper_loader.rb
+++ lib/lokka/theme_helper_loader.rb
@@ -4,7 +4,6 @@ module Lokka
       Dir["#{Lokka.root}/public/theme/*/helper/*_helper.rb"].each do |path|
         path = Pathname.new(path)
         lib  = path.parent.parent
-        root = lib.parent
         $:.push lib
         name = path.basename.to_s.split('.').first
         require "helper/#{name}"
